c689fa4ddd4594e88f2bbe1181ebb6d0375346e9
knucklesuganda <testaddressss43@gmail.com>
2023-03-07 19:51:17 +0100

    Assimilator Tutorial 3

--- main.py
+++ main.py
@@ -22,20 +22,47 @@ def create_user(uow: UnitOfWork):
 
 
 def read_user(repository: Repository):
-    user = repository.filter(
+    user = repository.get(
         repository.specs.filter(
-            email="python.on.papyrus@gmail.com",
+            username="Andrey",
         )
     )
 
     print(user)
 
 
+def update_user(uow: UnitOfWork):
+    with uow:
+        user = uow.repository.get(
+            uow.repository.specs.filter(
+                username="Andrey",
+            )
+        )
+
+        user.balance += 10
+        uow.repository.update(user)
+        uow.commit()
+
+        uow.repository.refresh(user)
+
+
+def delete_user(uow: UnitOfWork):
+    with uow:
+        user = uow.repository.get(
+            uow.repository.specs.filter(
+                username="Andrey-2",
+            )
+        )
+
+        uow.repository.delete(user)
+        uow.commit()
+
+
 if __name__ == '__main__':
-    try:
-        create_user(uow=create_uow())
-    except:
-        pass
-    read_user(
-        repository=create_repository(),
-    )
+    create_user(uow=create_uow())
+
+    read_user(repository=create_repository())
+    update_user(create_uow())
+    read_user(repository=create_repository())
+
+    delete_user(create_uow())
